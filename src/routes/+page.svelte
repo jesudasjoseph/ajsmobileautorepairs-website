bb0a97f6e0112d3c39e9c6adbd1886aec1f74f26
Jesudas Joseph <jesudasmjoseph@gmail.com>
2026-03-30 15:38:10 -0700

    Simple word change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1 class="mb-4 text-3xl font-bold">AJ's Mobile Auto Repairs</h1>
 
 <p class="mb-4">
-	Are you in and around Corvallis, OR? Do you have car troubles? Fill out the form below and I can
+	Are you in or around Corvallis, OR? Do you have car troubles? Fill out the form below and I can
 	come right to you and fix it!
 </p>
 
