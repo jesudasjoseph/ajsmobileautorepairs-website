968ebd2ca11980fbc937168bde66339f792983b8
Jesudas Joseph <jesudasmjoseph@gmail.com>
2026-03-30 15:33:20 -0700

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,9 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="min-h-dvh p-4 md:m-auto md:max-w-225">
+<div class="relative min-h-dvh p-4 md:m-auto md:max-w-225">
 	{@render children()}
+	<footer class="absolute right-0 bottom-0 left-0 p-4 text-center text-sm text-gray-500">
+		&copy; {new Date().getFullYear()} Jesudas Joseph. All rights reserved.
+	</footer>
 </div>
